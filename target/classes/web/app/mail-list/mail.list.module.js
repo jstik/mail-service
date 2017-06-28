734bcb7e6b6aa61cd801c5686680e7c0f5d6f9b7
jstik <jk.krylova@yandex.ru>
2017-06-28 18:50:12 +0300

    test commit

--- target/classes/web/app/mail-list/mail.list.module.js
+++
@@ -1,5 +0,0 @@
-let simpleTable = require('./../table/table.module');
-let ngRoute = require('./../bower_components/angular-route');
-let angularCSS = require('./../bower_components/angular-css');
-
-/*export default*/ angular.module('mailList', ['simpleTable','ngRoute','angularCSS']);
